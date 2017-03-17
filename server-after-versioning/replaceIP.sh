7295805580a0a161480696e844efa2b66d786e6b
Jonathan <jondef95@vt.edu>
2017-03-17 19:43:53 -0400

    added file to update server IP in files

---
+++ server-after-versioning/replaceIP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+LINE=$(ip address | grep '\eth0$')
+ADDRESS=$(echo $LINE | grep -o '[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}')
+
+sed -i "s/host=\".*\"/host=\"${ADDRESS}\"/g" swagger_server/__main__.py
+sed -i "s/host: \".*\"/host: \"http:\/\/${ADDRESS}:8080\"/g" swagger_server/swagger/swagger.yaml
