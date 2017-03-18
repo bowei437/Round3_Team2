6856700978dc11bd16782fe08873b504549cdff2
Jonathan <jondef95@vt.edu>
2017-03-17 20:14:07 -0400

    trying to fix port and script

--- server-after-versioning/replaceIP.sh
+++ server-after-versioning/replaceIP.sh
@@ -3,4 +3,4 @@ LINE=$(ip address | grep '\eth0$')
 ADDRESS=$(echo $LINE | grep -o '[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}')
 
 sed -i "s/host=\".*\"/host=\"${ADDRESS}\"/g" swagger_server/__main__.py
-sed -i "s/host: \".*\"/host: \"http:\/\/${ADDRESS}:8080\"/g" swagger_server/swagger/swagger.yaml
+sed -i "s/host: \".*\"/host: \"/${ADDRESS}:8080\"/g" swagger_server/swagger/swagger.yaml
