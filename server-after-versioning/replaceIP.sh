7be3b6346d08735c4661d47f1fa1ead06dd166c3
jondef95 <jondef95@vt.edu>
2017-03-19 23:28:55 -0400

    changing back

--- server-after-versioning/replaceIP.sh
+++ server-after-versioning/replaceIP.sh
@@ -2,6 +2,6 @@
 #LINE=$(ip address | grep '\eth0$')
 #ADDRESS=$(echo $LINE | grep -o '[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}')
 
-sed -i "s/host=\".*\"/host=\"0.0.0.0\"/g" swagger_server/__main__.py
+sed -i "s/host=\".*\"/host=\"${ADDRESS}\"/g" swagger_server/__main__.py
 sed -i "s/port=[0-9][0-9][0-9][0-9]/port=8080/g" swagger_server/__main__.py
-sed -i "s/host: \".*\"/host: \"ec2-54-190-2-178.us-west-2.compute.amazonaws.com:8080\"/g" swagger_server/swagger/swagger.yaml
+sed -i "s/host: \".*\"/host: \"${ADDRESS}\"/g" swagger_server/swagger/swagger.yaml
