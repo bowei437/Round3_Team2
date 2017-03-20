738445893d585951c43e9a0b7d2fb2c1e97da1fc
jondef95 <jondef95@vt.edu>
2017-03-20 01:00:08 -0400

    Update replaceIP.sh

--- server-after-versioning/replaceIP.sh
+++ server-after-versioning/replaceIP.sh
@@ -2,6 +2,6 @@
 #LINE=$(ip address | grep '\eth0$')
 #ADDRESS=$(echo $LINE | grep -o '[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}')
 
-sed -i "s/host=\".*\"/host=\"ec2-54-190-2-178.us-west-2.compute.amazonaws.com\"/g" swagger_server/__main__.py
+sed -i "s/host=\".*\"/host=\"0.0.0.0\"/g" swagger_server/__main__.py
 sed -i "s/port=[0-9][0-9][0-9][0-9]/port=8080/g" swagger_server/__main__.py
 #sed -i "s/host: \".*\"/host: \"${ADDRESS}:8080\"/g" swagger_server/swagger/swagger.yaml
