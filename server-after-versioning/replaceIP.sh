bbb6ae90e5f053085de5e9a956fb1dbefc8e7b32
jondef95 <jondef95@vt.edu>
2017-03-19 23:30:16 -0400

    Update replaceIP.sh

--- server-after-versioning/replaceIP.sh
+++ server-after-versioning/replaceIP.sh
@@ -4,4 +4,4 @@ ADDRESS=$(echo $LINE | grep -o '[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\{0,3\}\.[0-9]\
 
 sed -i "s/host=\".*\"/host=\"${ADDRESS}\"/g" swagger_server/__main__.py
 sed -i "s/port=[0-9][0-9][0-9][0-9]/port=8080/g" swagger_server/__main__.py
-sed -i "s/host: \".*\"/host: \"${ADDRESS}\"/g" swagger_server/swagger/swagger.yaml
+sed -i "s/host: \".*\"/host: \"${ADDRESS}:8080\"/g" swagger_server/swagger/swagger.yaml
