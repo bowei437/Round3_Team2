3149bdc132a154b765451ccbe1281d9ff0249546
jondef95 <jondef95@vt.edu>
2017-03-23 22:09:03 -0400

    all controller code has been moved over, still need validation added

--- python-flask-server/swagger_server/controllers/problems_controller.py
+++ python-flask-server/swagger_server/controllers/problems_controller.py
@@ -18,7 +18,28 @@ def add_problem():
 
     :rtype: int
     """
-    return 'do some magic!'
+    response = requests.post(storage_url, json=default_problem)
+
+    #check if storage comes back okay
+    if (response.status_code != 200):
+        return jsonify(Error(500, "Error in storage")), HTTP_500_INTERNAL_SERVER_ERROR
+    
+    #retrieve the problem_id
+    json_response = response.json()
+    problem_id = json_response['problem_id']
+    #PUT the default Problem JSON into storage
+    params = "id=%s/ver=%s/" % (str(problem_id),"0")
+    put_url = storage_url + str(params)
+    default_problem['problem_id'] = problem_id
+    response = requests.put(put_url, json=default_problem)
+
+
+    #check that Storage didn't die in some way
+    if (response.status_code != 200):
+        return jsonify(Error(500, "Error in storage")), status.HTTP_500_INTERNAL_SERVER_ERROR
+    
+    #return the default problem so the user knows it has been created
+    return (jsonify(default_problem)), status.HTTP_201_CREATED
 
 
 def delete_problem(problem_id):
@@ -30,7 +51,25 @@ def delete_problem(problem_id):
 
     :rtype: None
     """
-    return 'do some magic!'
+    #make sure ID was valid
+    if (problem_id < 0):
+        return jsonify(Error(400, "Problem not valid")), status.HTTP_400_BAD_REQUEST
+    
+    #contact Storage
+    params = "id=%s/" % str(problem_id)
+    url = storage_url + str(params)
+    get_response = requests.delete(url)
+
+    #make sure Problem existed
+    if (get_response.status_code == 404):
+        return jsonify(Error(404, "Problem not found")), status.HTTP_404_NOT_FOUND
+
+    #check if the Storage died
+    elif (get_response.status_code != 200):
+        return jsonify(Error(500, "Storage server error")), status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    #return response from Storage
+    return jsonify({"response": "successfully deleted"})
 
 
 def get_problem(problem_id):
@@ -42,4 +81,21 @@ def get_problem(problem_id):
 
     :rtype: Problem
     """
-    return 'do some magic!'
+    #make sure ID was valid
+    if (problem_id < 0):
+        return jsonify(Error(400, "Problem not valid")), status.HTTP_400_BAD_REQUEST
+
+    #contact Storage
+    params = "id=%s/" % str(problem_id)
+    get_url = storage_url + str(params)
+    response = requests.get(get_url)
+
+    #make sure Problem existed
+    if (response.status_code == 404):
+        return jsonify(Error(404, "Problem not found")), status.HTTP_404_NOT_FOUND
+    #check if Storage died
+    elif (response.status_code != 200):
+        return jsonify(Error(response.status_code, "Storage server error")), status.HTTP_500_INTERNAL_SERVER_ERROR
+    #return response from Storage
+    return jsonify(response.json()["body"])
+
