8ab0dc9cc48c948e5f2dc9ff5a23040433b26784
jth2279 <jth2279@users.noreply.github.com>
2017-03-27 15:30:04 -0400

    Update path_controller.py

--- python-flask-server/swagger_server/controllers/path_controller.py
+++ python-flask-server/swagger_server/controllers/path_controller.py
@@ -9,7 +9,7 @@ import requests, json
 from flask import jsonify
 from flask_api import status
 
-storage_url = "http://storage_container:8000/v2/"
+storage_url = "http://storage_container:8082/v2/"
 
 def get_path(problem_id):
     """
